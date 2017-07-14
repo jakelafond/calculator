ddadfcff3c15ac0cf7a611c31a80f02c39492a09
Jake LaFond <jakelafond@gmail.com>
2017-07-14 15:12:18 -0400

    Hardmode done

--- main.js
+++ main.js
@@ -6,12 +6,13 @@ var numButtons = document.querySelectorAll('.button_num');
 var tempHold = '';
 var viewer = document.querySelector('#answer');
 var operator = document.querySelectorAll('.button_op');
-var calculate = document.querySelectorAll('.button_fin');
-var clear = document.querySelectorAll('.clear');
-var specialMod = document.querySelectorAll('.button_op_mod');
-var specialSqrt = document.querySelectorAll('.button_op_sqrt');
+var calculate = document.querySelector('.button_fin');
+var clear = document.querySelector('.clear');
+var specialMod = document.querySelector('.button_op_mod');
+var specialSqrt = document.querySelector('.button_op_sqrt');
+var prvCalcs = document.querySelector('.previous_calcs');
 
-//do math stuff,
+//do math stuff
 
 for (let i = 0; i < numButtons.length; i++) {
   let buttn = numButtons[i];
@@ -31,42 +32,39 @@ for (let i = 0; i < operator.length; i++) {
   });
 }
 
-for (let i = 0; i < specialMod.length; i++) {
-  let buttn = specialMod[i];
-  buttn.addEventListener('click', () => {
-    let content = '%';
-    tempHold += content;
-    viewer.textContent = tempHold;
-  });
-}
-for (let i = 0; i < specialSqrt.length; i++) {
-  let buttn = specialSqrt[i];
-  buttn.addEventListener('click', () => {
-    let content = 'sqrt';
-    tempHold += content;
-    viewer.textContent = tempHold;
-  });
-}
+specialMod.addEventListener('click', () => {
+  let content = '%';
+  tempHold += content;
+  viewer.textContent = tempHold;
+});
 
-for (let i = 0; i < calculate.length; i++) {
-  let buttn = calculate[i];
-  buttn.addEventListener('click', () => {
-    if (tempHold.includes('sqrt')) {
-      let sqrtSplit = tempHold.split('sqrt');
-      let sqrtTotal = Math.sqrt(Number(sqrtSplit[0]) + Number(sqrtSplit[1]));
-      viewer.textContent = sqrtTotal;
-    } else {
-      let finish = eval(tempHold);
-      tempHold = finish;
-      viewer.textContent = finish;
-    }
-  });
-}
+specialSqrt.addEventListener('click', () => {
+  let content = 'sqrt';
+  tempHold += content;
+  viewer.textContent = tempHold;
+});
 
-for (let i = 0; i < clear.length; i++) {
-  let buttn = clear[i];
-  buttn.addEventListener('click', () => {
-    tempHold = '';
-    viewer.textContent = tempHold;
-  });
-}
+calculate.addEventListener('click', () => {
+  if (tempHold.includes('sqrt')) {
+    let oldCalc = tempHold;
+    let sqrtSplit = tempHold.split('sqrt');
+    let sqrtTotal = Math.sqrt(Number(sqrtSplit[0]) + Number(sqrtSplit[1]));
+    viewer.textContent = sqrtTotal;
+    var newCalc = document.createElement('p');
+    newCalc.textContent = `${oldCalc} = ${sqrtTotal}`;
+    prvCalcs.appendChild(newCalc);
+  } else {
+    let oldCalc = tempHold;
+    let finish = eval(tempHold);
+    tempHold = finish;
+    viewer.textContent = finish;
+    var newCalc = document.createElement('p');
+    newCalc.textContent = `${oldCalc} = ${finish}`;
+    prvCalcs.appendChild(newCalc);
+  }
+});
+
+clear.addEventListener('click', () => {
+  tempHold = '';
+  viewer.textContent = tempHold;
+});
